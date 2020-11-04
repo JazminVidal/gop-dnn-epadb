897582effac1c6533b77a0fb85b3c4696a274905
JazminVidal <37957976+JazminVidal@users.noreply.github.com>
2020-11-04 10:21:20 -0300

    Add files via upload

---
+++ path.sh
@@ -0,0 +1,14 @@
+export KALDI_ROOT=/Users/jazmin/Desktop/kaldi
+export EPADB_ROOT=/Users/jazmin/Desktop/epadb
+export MODEL_ROOT=/Users/jazmin/Desktop/gop-dnn-epadb/0013_librispeech_v1
+
+
+[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
+
+export PATH=$KALDI_ROOT/egs/wsj/s5/utils:$KALDI_ROOT/egs/wsj/s5/step:$KALDI_ROOT/egs/gop/s5/local:$KALDI_ROOT/tools/openfst/bin:$PATH
+
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+
+. $KALDI_ROOT/tools/config/common_path.sh
+
+export LC_ALL=C
